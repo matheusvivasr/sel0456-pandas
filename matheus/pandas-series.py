1f010421ba0cc470719d0be36d0984a60a3f2fc7
matheusvivasr <vivas.matheus@usp.br>
2022-11-08 13:56:01 -0300

    adjustments

--- matheus/pandas-series.py
+++ matheus/pandas-series.py
@@ -2,25 +2,32 @@ import pandas as pd
 import numpy as np
 
 a = pd.Series([1,2,3,4,5],index=["a","b","c","d","e"])
-print(a)
+#print(a)
 
-#print(a*2)
+#print(a[2])
 #print(a["c"])
+#print(a*2)     # entre outras operacoes '+' '-' '/' 
 #print(a[:2])
 #print(a[-2:])
+
 
 dict = {"a":1,"b":2,"c":3,"d":4,"e":5}
 b = pd.Series(dict, index=["c","b","X","a","d","e"])
 #print(b)
-#print(b["c"])
-#print(b.count(),'aa')
-#print(b.size)
+
+#print("quantidade de números:",b.count())      # NaN não é contado
+#print("quantidade de elementos:",b.size)
+
 
 c = pd.Series([[1, 2, 3], 'foo', [], [3, 4]])
+#print(c)
+#print(c.explode())     # observe os indices
 #print(c.explode(ignore_index = True))
 
 d = pd.Series([4+3j,-1,4,1.3+3j])
+#print(d)
 #print(d.abs())
 
 e = pd.Series([0, 1, np.nan, 3])
-#print(e.interpolate())
+#print(e)
+#print(e.interpolate(method='linear'))  #'linear', 'polynomial', 'from_derivatives'
